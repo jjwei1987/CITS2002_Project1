009851acf34ddbb34a848713dd72752a2a16012d
jjwei1987 <127012084+jjwei1987@users.noreply.github.com>
2023-09-10 20:22:15 +0800

    Add files via upload
    updated myscheduler, read sysconfig and read comment added, execute command structure in progress

--- myscheduler.c
+++ myscheduler.c
@@ -5,13 +5,9 @@
 
 
 //  CITS2002 Project 1 2023
-//  Student1:   STUDENT-NUMBER1   NAME-1
-//  Student2:   STUDENT-NUMBER2   NAME-2
-
-
-//  myscheduler (v1.0)
-//  Compile with:  cc -std=c11 -Wall -Werror -o myscheduler myscheduler.c
-
+//  Student1:   21022228    Jie Wei
+//  Student2:   21143691    Amanda Brown
+//  Date:       2023-09-15
 
 //  THESE CONSTANTS DEFINE THE MAXIMUM SIZE OF sysconfig AND command DETAILS
 //  THAT YOUR PROGRAM NEEDS TO SUPPORT.  YOU'LL REQUIRE THESE //  CONSTANTS
@@ -49,6 +45,7 @@ int ndevices = 0;                                  //  The number of I/O devices
 int ncommands = 0;                                 //  The number of commands
 int nprocesses = 0;                                //  The number of processes that have been created
 int nsyscalls = 0;                                 //  The number of system calls that have been made
+int time_quantum = DEFAULT_TIME_QUANTUM;                                  //  The time quantum
 
 //  ----------------------------------------------------------------------
 //  STRUCTURES
@@ -61,13 +58,16 @@ struct sysconfig{
 struct commands{
     int        when_evoked;
     char       command_name[MAX_COMMAND_NAME];
-    struct{
+    int        nsyscalls;
+    struct
+    {
         int         run_time;
-        char        syscall_name;
-        char        device_name_of_syscall;
+        char        syscall_name[MAX_COMMAND_NAME];
+        char        device_name_of_syscall[MAX_DEVICE_NAME];
         int         syscall_argument;
+        char        child_command_name[MAX_COMMAND_NAME];
     }syscalls[MAX_SYSCALLS_PER_PROCESS];
-} commands[MAX_COMMANDS];
+} command_list[MAX_COMMANDS];
 
 struct queue{
     char queue_name[MAX_COMMAND_NAME];
@@ -75,44 +75,86 @@ struct queue{
     int end;
 }processes[MAX_RUNNING_PROCESSES];
 // ----------------------------------------------------------------------
-//  FUNCTIONS
+//  Queue and FUNCTIONS
 
 void init_queue(struct queue *queue){
     queue->front = -1;
     queue->end = -1;
 }
 
-void is_queue_empty(struct queue *queue)
+int is_queue_empty(struct queue *queue)
 {
-    if(queue->processes[0].process_id == 0)
+    if(queue->front == -1)
     {
         printf("Queue is empty\n");
     }
-    else
+}
+
+int is_queue_full(struct queue *queue)
+{
+    if(queue->end = MAX_RUNNING_PROCESSES -1 && queue->front == 0)
     {
-        printf("Queue is not empty\n");
+        printf("Queue is full\n");
+    }
+    {
+        printf("Queue is full\n");
     }
 }
 
-void is_queue_full(struct queue *queue)
+void addToReadyQueue(struct queue *queue, int *command) 
 {
-    if(queue->processes[MAX_RUNNING_PROCESSES].process_id != 0)
+    //  Append all commands to the ready_queue array
+    if(is_queue_full(queue))
     {
         printf("Queue is full\n");
     }
     else
     {
-        printf("Queue is not full\n");
+        if(queue->front == -1)
+        {
+            queue->front = 0;
+        }
+        queue->end = (queue->end + 1) % MAX_RUNNING_PROCESSES;
+        queue->queue_name[queue->end] = command;
+        printf("Command %i added to ready queue\n", command);
     }
 }
 
-int enqueue(){
 
+void moveFromReadyToRunning(struct queue *ready_queue, struct queue *running_queue) 
+{
+    // Move a process from ready_queue to running
+}
+/*
+    Loop through ready_queue
+    append i to running, del i from ready_queue
+    add 5 to counter
+ */
+
+
+void executeProcess(/* Parameters */) {
+    // Implement the main logic for process execution
+}
+/*  while loop for the time quantum
+    if statement for process exit if cpu_counter == total expected execution time
+    else, add execute time to the counter and cpu_counter
+ */
+
+void handleSystemCall(/* Parameters */) {
+    // Implement logic to handle system calls (e.g., sleep, read, write, etc.)
 }
 
-int dequeue(){
 
+void addToBlockedQueue(/* Parameters */) {
+    // Add a process to the appropriate blocked queue (sleep_blocked_queue, IO_blocked_queue, parent_blocked_queue)
 }
+
+void moveToReadyQueue(/* Parameters */) {
+    // Move a process from a blocked queue to the ready_queue
+}
+
+// ----------------------------------------------------------------------
+// FUNCTIONS TO READ THE SYSTEM CONFIGURATION FILE AND COMMAND FILE
 
 void read_sysconfig(char argv0[], char filename[])
 {
@@ -120,48 +162,242 @@ void read_sysconfig(char argv0[], char filename[])
 
     if(sysconfig != NULL)
     {
-        char line[BUFSIZE];
-        while(fgets(line, sizeof(line), sysconfig) != NULL)
+        char sys_line[BUFSIZE];
+        while(fgets(sys_line, BUFSIZE, sysconfig) != NULL)
         {
-            if(line[0] == CHAR_COMMENT)
+            if(sys_line[0] == CHAR_COMMENT)
             {
                 continue;
             }
-            else
+            
+            
             {
                 char device_name[MAX_DEVICE_NAME];
                 int read_speed;
                 int write_speed;
-                sscanf(line, "%s %i %i", device_name, &read_speed, &write_speed);
-                strcpy(devices[ndevices].device_name, device_name);
-                devices[ndevices].read_speed = read_speed;
-                devices[ndevices].write_speed = write_speed;
-                ndevices++;
+                // read the device name, read speed and write speed, skip the first string, devices array size is 3
+                if ((sscanf(sys_line, "%*s %s %i %*s %i", device_name, &read_speed, &write_speed) == 3))
+                {
+                    strcpy(devices[ndevices].device_name, device_name);
+                    devices[ndevices].read_speed = read_speed;
+                    devices[ndevices].write_speed = write_speed;
+                    ndevices++;
+                    printf("%i %s\t %i\t %i\n", ndevices, device_name, read_speed, write_speed);
+                }
+                
             }
+
+            if(sys_line[0] == 't')
+            {
+                if(sscanf(sys_line, "%*s %i", &time_quantum) == 1)
+                {
+                    printf("Time Quantum: %i\n", time_quantum);
+                }
+            } 
         }
     }
-    else
-    {
-        printf("Error: Cannot open file %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
+    fclose(sysconfig);
 
 }
 
 void read_commands(char argv0[], char filename[])
 {
-}
+    FILE *commands = fopen(filename, "r");
 
+    if (commands != NULL)
+    {
+        char command_line[BUFSIZE];
+        while (fgets(command_line, BUFSIZE, commands) != NULL)
+        {
+        //skip the "#" line
+            if (command_line[0] == CHAR_COMMENT)
+            {
+                continue;
+            }
+        //read command name, 1 string
+            if(command_line[0] != ' ' && command_line[0] != CHAR_COMMENT)
+            {
+                if(sscanf(command_line, "%s", command_list[ncommands].command_name) == 1)
+                {
+                    printf("Command %i %s\n", ncommands, command_list[ncommands].command_name);
+                
+                
+                    while (fgets(command_line, BUFSIZE, commands))
+                    {
+                        
+                        //read normal system call (3), 1 integer, usecs, 1 string, 1 integer
+                        if(sscanf(command_line, "%i usecs %s %i usecs", 
+                                &command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].run_time, 
+                                command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].syscall_name, 
+                                &command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].syscall_argument) == 3)
+                        {
+                            printf("%i-%i %i %s %i usecs\n",
+                                    ncommands,
+                                    command_list[ncommands].nsyscalls, 
+                                    command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].run_time, 
+                                    command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].syscall_name, 
+                                    command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].syscall_argument);
+                            command_list[ncommands].nsyscalls++;
+                        }                        
+
+                        //read IO system call (4), 1 integer, usecs, 1 string, 1 string, 1 integer
+                        else if(sscanf(command_line, "%i usecs %s %s %i B", 
+                                &command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].run_time,
+                                command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].syscall_name,
+                                command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].device_name_of_syscall,
+                                &command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].syscall_argument) == 4)
+                        {
+                            printf("%i-%i %i %s %s %i\n",
+                                    ncommands,
+                                    command_list[ncommands].nsyscalls,
+                                    command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].run_time,
+                                    command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].syscall_name,
+                                    command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].device_name_of_syscall,
+                                    command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].syscall_argument);
+                            command_list[ncommands].nsyscalls++;
+                        }
+
+                        //read spawn call
+                        else if(sscanf(command_line, "%i usecs %s %s", 
+                                &command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].run_time, 
+                                command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].syscall_name, 
+                                command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].child_command_name) == 3)
+                        {
+                            printf("%i-%i %i %s %s\n",
+                                    ncommands,
+                                    command_list[ncommands].nsyscalls,
+                                    command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].run_time, 
+                                    command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].syscall_name, 
+                                    command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].child_command_name);
+                            command_list[ncommands].nsyscalls++;
+                        }
+
+                        //read exit and wait call (2), 1 integer, 1 strings
+                        else if(sscanf(command_line, "%i usecs %s", 
+                                &command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].run_time, 
+                                command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].syscall_name) == 2)
+                        {
+                            printf("%i-%i %i %s\n",
+                                    ncommands, 
+                                    command_list[ncommands].nsyscalls,
+                                    command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].run_time, 
+                                    command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].syscall_name);
+
+                            if(strcmp(command_list[ncommands].syscalls[command_list[ncommands].nsyscalls].syscall_name, "exit") == 0)
+                            {
+                                break;
+                            }
+                        }
+
+
+                    
+                        else
+                        {
+                            printf("Error: Invalid command\n");
+                        }
+                    }
+                    ncommands++;   
+
+                }    
+            }
+        }
+    }
+    fclose(commands);
+}
 //  ----------------------------------------------------------------------
+//  FUNCTIONS TO SIMULATE THE SYSTEM'S OPERATION
+//  Running -> Exit        clock 0
 //  Ready -> Running       clock +5
 //  Running -> Blocked     clock +10
 //  Running -> Ready       clock +10
 //  Blocked -> Ready       clock +10
 //  All process -> first acquires data-bus (clock +20)
+//  Process that exhaust their time quantum always return to the ready queue
+//  When the CPU is idle (no process or the OS is using the CPU), perform the following in this order:
+//  unblock any sleeping processes
+//  unblock any processes waiting for all their spawned processes (children) to terminate
+//  unblock any completed I/O
+//  commence any pending I/O
+//  commence/resume the next READY process
+//  otherwise the CPU remains idle...
+
 void execute_commands(void)
 {
-}
+    //  Step 1: Create ready queue, blocked queue, running queue
+    struct queue ready_queue;
+    struct queue blocked_queue;
+    struct queue running_queue;
+    init_queue(&ready_queue);
+    init_queue(&blocked_queue);
+    init_queue(&running_queue);
 
+
+    //  Step 2: loop through the commands
+    //  add commands to ready queue and print out the content of the ready queue
+    //  move commands from ready queue to running queue, CLOCK + 5
+    //  update the global clock to account for the time taken to move the syscalls from ready to running to the CPU
+    //  execute first syscall of the process for run_time, CLOCK + run_time
+    //  nsyscalls + 1
+    //  update the total time that all syscalls have spent on the CPU, TOTAL_PROCESS_TIME_ON_CPU = syscall_argument + TOTAL_PROCESS_TIME_ON_CPU
+    //  print out the content of the running queue
+    //  print information about the command being executed, including its PID, the command name, and the time it was started
+    //  check if the command is a blocking command (sleep or wait), if it is, move it to the blocked queue, block time is the time
+    //  if the command is not a blocking command, move it to the ready queue, clock + 10
+    //  if the command run_time is over thank time quantum, move it to the ready queue, clock + 10
+    //  check next command check if the command is spawn, if it is, create a new process for the child command, add the child command to the ready queue, clock + 10
+    //  check next command check if the command is exit, if it is, move the command out of running queue, clock + 0
+    //  when nprocesses == 0, exit the loop
+    for(int i = 0; i < ncommands; i++)
+    {
+        addToReadyQueue(&ready_queue, &command_list[i]);
+        printf("Ready Queue: %s\n", ready_queue.queue_name);
+
+        nprocesses++;
+        moveFromReadyToRunning(&ready_queue, &running_queue);
+        GLOBAL_CLOCK += 5;
+        printf("Running Queue: %s\n", running_queue.queue_name);
+        printf("Command %i %s started at %i\n", i, command_list[i].command_name, GLOBAL_CLOCK);
+        for(int j = 0; j < command_list[i].nsyscalls; j++)
+        {
+            GLOBAL_CLOCK += command_list[i].syscalls[j].run_time;
+            TOTAL_PROCESS_TIME_ON_CPU += command_list[i].syscalls[j].syscall_argument;
+            printf("Command %i %s %i %s %i\n", i, command_list[i].command_name, j, command_list[i].syscalls[j].syscall_name, GLOBAL_CLOCK);
+            if(strcmp(command_list[i].syscalls[j].syscall_name, "sleep") == 0 || strcmp(command_list[i].syscalls[j].syscall_name, "wait") == 0)
+            {
+                addToBlockedQueue(&blocked_queue, &command_list[i]);
+                printf("Blocked Queue: %s\n", blocked_queue.queue_name);
+            }
+            else
+            {
+                addToReadyQueue(&ready_queue, &command_list[i]);
+                printf("Ready Queue: %s\n", ready_queue.queue_name);
+                GLOBAL_CLOCK += 10;
+            }
+            if(command_list[i].syscalls[j].run_time > time_quantum)
+            {
+                addToReadyQueue(&ready_queue, &command_list[i]);
+                printf("Ready Queue: %s\n", ready_queue.queue_name);
+                GLOBAL_CLOCK += 10;
+            }
+            if(strcmp(command_list[i].syscalls[j].syscall_name, "spawn") == 0)
+            {
+                // create a new process for the child command
+                // add the child command to the ready queue
+                // clock + 10
+                addToReadyQueue(&ready_queue, &command_list[i]);
+                printf("Ready Queue: %s\n", ready_queue.queue_name);
+                GLOBAL_CLOCK += 10;
+            }
+
+            // move the command out of running queue, clock + 0
+            if(strcmp(command_list[i].syscalls[j].syscall_name, "exit") == 0)
+            {
+
+            }
+        }
+    }
+
+}
 //  ----------------------------------------------------------------------
 // main() function to start the program
 
@@ -173,21 +409,15 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-//    verbose = verbose || (getenv("HTTPS") != NULL);
-
 //  READ THE SYSTEM CONFIGURATION FILE
     read_sysconfig(argv[0], argv[1]);
-    dump_sysconfig();
-    exit(0);
 
 //  READ THE COMMAND FILE
     read_commands(argv[0], argv[2]);
-    dump_commands();
-    exit(0);
 
-    init_processes();
-    init_READY_queue();
-    init_BLOCKED_queue();
+//   init_processes();
+//    init_READY_queue();
+//    init_BLOCKED_queue();
 
 //  EXECUTE COMMANDS, STARTING AT FIRST IN command-file, UNTIL NONE REMAIN
     execute_commands();
